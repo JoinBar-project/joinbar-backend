cc42cd43f361093e7d65534d29f301fd3ad08147
LAPTOP-7I4N09T2\Bella <dhp951320@gmail.com>
2025-05-31 21:31:10 +0800

    feat : move files to the src folder

--- src/drizzle/drizzle.config.js
+++ src/drizzle/drizzle.config.js
@@ -4,8 +4,8 @@ const { defineConfig } = require('drizzle-kit');
 dotenv.config();
 
 module.exports = defineConfig({
-  schema: "./src/models/schema.js",
-  out: "./src/drizzle/migrations",
+  schema: "../models/schema",
+  out: "../drizzle/migrations",
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL,
