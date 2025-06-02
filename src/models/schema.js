e3a1339b629602e1d7c76eddcd0694cd08cabb78
jasminechen95 <tingone59@gmail.com>
2025-06-02 19:25:23 +0800

    feat: add tags column and adjust rating precision in barsTable

--- src/models/schema.js
+++ src/models/schema.js
@@ -34,7 +34,7 @@ const barsTable = pgTable("bars", {
   address: varchar({ length: 255 }),
   phone: varchar({ length: 20 }),
   description: text(),
-  rating: numeric("rating", { precision: 3, scale: 1 }), // 計算至小數點後一位
+  rating: numeric("rating", { precision: 2, scale: 1 }), // 計算至小數點後一位
   openHours: varchar("open_hours", { length: 50 }),
   createdAt: timestamp("created_at").defaultNow(),
 });
