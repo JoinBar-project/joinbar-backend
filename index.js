24b9b6870c2b92d06a9f390d6187f4ace468c751
Rean Tsai <aobc55625@gmail.com>
2025-06-03 11:52:16 +0800

    feat: integrate LINE authentication routes into main server

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const express = require('express');
 const dotenv = require('dotenv');
 const eventRoutes = require('./src/routes/eventRoutes');
 const tagsRoutes = require('./src/routes/tagsRoutes');
+const cors = require('cors');
+const { corsOptions } = require('./src/config/cors');
+const lineAuthRoutes = require("./src/routes/lineAuthRoutes");
 
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
+app.use("/api/auth/line", lineAuthRoutes);
 app.use('/event', eventRoutes);
 app.use('/tags', tagsRoutes);
 
 
 app.listen(3000, () => {
   console.log('伺服器已啟動 http://localhost:3000');
+  console.log(`LINE Auth URL: http://localhost:3000/api/auth/line/url`)
 });
 
