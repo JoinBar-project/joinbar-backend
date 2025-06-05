06e29425ad488399e524c0dfddd1abd75ebd8f14
2xin15 <s111291519xin@gapp.nthu.edu.tw>
2025-06-05 22:29:42 +0800

    feat: add addTags post from API function

--- src/controllers/barTagControllers.js
+++ src/controllers/barTagControllers.js
@@ -1,38 +1,41 @@
 const db = require('../config/db');
 // tags, barTags：從 schema 匯入的兩張資料表。barTags 是多對多關聯表（bar 和 tag 的對應關係）
-const { tags, eventTags } = require('../models/schema');
+const { userTags } = require('../models/schema');
 const { eq } = require('drizzle-orm');
 
-// 新增標籤到酒吧
-const addTagsToBar = async (req, res) => {
-  // 轉成 10 進位整數
-  const barId = parseInt(req.params.barId, 10);
-  const tagId = req.body.tagId;
+// 新增標籤到個人推薦
+const addTagsToUser = async (req, res) => {
+  const {UserId ,sport, music, student, bistro, drink, joy, romantic, oldschool, highlevel, easy} = req.body;
 
-  if (!Array.isArray(tagId) || tagId.length === 0) {
-    return res.status(400).json({ message: '請提供 tagIds 陣列'});
-  }
+  console.log('user_id:', UserId); 
 
-  try{
-    const barTagData = tagId.map((tagId) => ({
-      barId,
-      tagId
-    }));
+  try {
+    const newUserTags = await db.insert(userTags).values({
+      UserId,  
+      sport,
+      music,
+      student,
+      bistro,
+      drink,
+      joy,
+      romantic,
+      oldschool,
+      highlevel,
+      easy,
+    });
 
-    await db.insert(barTags).values(barTagData);
-    return res.status(201).json({ message: '已新增標籤到酒吧', tags: tagIds });
+    return res.status(201).json({ message: "新增成功"});
   } catch (err) {
-    console.error('新增標籤失敗：',err);
-    return res.status(500).json({ message: '伺服器錯誤' });
+    return res.status(500).json({ error: err.message });
   }
 };
 
-const getBarTags = async (req, res) => {
+// const getBarTags = async (req, res) => {
   
-}
+// }
 
-const removeTagFromBar = async (req, res) => {
+// const removeTagFromBar = async (req, res) => {
   
-}
+// }
 
-module.exports = { addTagsToBar, getBarTags, removeTagFromBar };
+module.exports = { addTagsToUser };
--- src/routes/barTagsRoutes.js
+++ src/routes/barTagsRoutes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { addTagsToBar, getBarTags, removeTagFromBar } = require("../controllers/barTagControllers.js");
-const { route } = require("./tagsRoutes.js");
-const { verifyToken, checkAdmin } = require("../middlewares/checkRole.js");
-const { tags } = require("../models/schema.js");
+const { addTagsToUser } = require("../controllers/barTagControllers.js");
 
-router.post("/bars/:barId/tags", verifyToken, checkAdmin, addTagsToBar );
-router.get("/bars/:barId/tags", getBarTags);
-router.delete("/bars/:barId/tags/:tagId", verifyToken, checkAdmin, removeTagFromBar);
+
+router.post("/addTags", addTagsToUser);
+// router.get("/bars/:barId/tags", getBarTags);
+// router.delete("/bars/:barId/tags/:tagId", verifyToken, checkAdmin, removeTagFromBar);
 
 module.exports = router;
