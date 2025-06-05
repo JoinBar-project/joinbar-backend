4d3f7851d16a6a786d0783bcd02f9b795951e45b
Rean Tsai <aobc55625@gmail.com>
2025-06-05 15:17:29 +0800

    refactor: update middleware import path and apply database migration

--- src/routes/lineAuthRoutes.js
+++ src/routes/lineAuthRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getLineAuthUrl, lineCallback, lineLogout } = require('../controllers/lineAuthController');
-const { authenticateToken } = require('../middleware/authenticate');
+const { authenticateToken } = require('../middlewares/authenticateToken');
 
 // 取得 LINE 授權 URL (公開路由)
 router.get('/url', getLineAuthUrl);
