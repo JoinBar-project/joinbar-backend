c18a0579776b4ee6f24dd77c920d5e352695877e
Bella-Cheng <dhp951320@gmail.com>
2025-06-09 16:14:35 +0800

    feat : using function in dateFormatte.js

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -8,3 +8,31 @@ dayjs.extend(timezone);
 const tz = 'Asia/Taipei';
 
 const allowedFields = ['startDate', 'endDate', 'createdAt', 'modifyAt'];
+
+function formatToTaiwanTime(data) {
+  if (Array.isArray(data)) {
+    return data.map(formatToTaiwanTime);
+  }
+
+  if (data && typeof data === 'object') {
+    const result = {};
+    for (const key in data) {
+      const value = data[key];
+
+      if (allowedFields.includes(key) && (value instanceof Date || (typeof value === 'string' && dayjs(value).isValid()))) {
+        result[key] = dayjs.utc(value).tz(tz).format('YYYY-MM-DD HH:mm:ss');
+      } else if (typeof value === 'object') {
+        result[key] = formatToTaiwanTime(value);
+      } else {
+        result[key] = value;
+      }
+    }
+    return result;
+  }
+
+  return data;
+}
+
+module.exports = {
+  formatToTaiwanTime,
+};
