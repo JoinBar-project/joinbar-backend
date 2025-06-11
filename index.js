60cf6042c0c3995032257b5a78cce518758e944b
Ben <ab4827qq@gmail.com>
2025-06-11 12:45:36 +0800

    remove: uninstall cors package

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./src/routes/authRoutes');
 const eventRoutes = require('./src/routes/eventRoutes');
@@ -9,18 +8,6 @@ const orderRoutes = require('./src/routes/orderRoutes');
 dotenv.config();
 
 const app = express();
-
-app.use(cors({
-  origin: [
-    'http://localhost:5173',    
-    'http://localhost:3000',    
-    'http://localhost:8080',    
-    'http://127.0.0.1:5173'     
-  ],
-  credentials: true,            
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 app.use(express.json());
 
