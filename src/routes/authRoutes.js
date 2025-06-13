28448c0e1bdb59c62dc2e4a402db6571c5efb799
Rean Tsai <aobc55625@gmail.com>
2025-06-14 02:00:24 +0800

    feat: add email verification route to auth endpoints

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { signup, login } = require("../controllers/authControllers.js");
+const { signup, login, verifyEmail } = require("../controllers/authControllers.js");
 const  validateSignup  = require("../middlewares/validateSignup.js");
 
 
 router.post("/signup", validateSignup, signup);
+router.get("/verify-email", verifyEmail);
 router.post("/login", login);
 
 module.exports = router;
