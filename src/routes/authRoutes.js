6cbb074b3abb467deb3bf077ed096d5fc5b99cd1
Rean Tsai <aobc55625@gmail.com>
2025-06-14 02:58:02 +0800

    feat: add resend verification email route to auth endpoints

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { signup, login, verifyEmail } = require("../controllers/authControllers.js");
+const { signup, login, verifyEmail, resendVerificationEmail } = require("../controllers/authControllers.js");
 const  validateSignup  = require("../middlewares/validateSignup.js");
 
 
 router.post("/signup", validateSignup, signup);
 router.get("/verify-email", verifyEmail);
+router.post("/resend-verification", resendVerificationEmail);
 router.post("/login", login);
 
 module.exports = router;
