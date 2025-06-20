75342dee6c8074e3da94f42e622171a9797442ad
Rean Tsai <aobc55625@gmail.com>
2025-06-20 15:16:59 +0800

    Remove drizzle migrations before merge

--- src/drizzle/migrations/0004_lovely_ultron.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "messages" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"content" text NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now(),
-	"user_id" integer NOT NULL,
-	"event_id" bigint NOT NULL
-);
